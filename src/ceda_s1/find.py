2ba08096d27d52297aac71a55135afce7dc394e2
Tom Wilson <thomaswilson81@gmail.com>
2025-03-25 11:48:47 +0000

    S1 downloads v2 (#6)
    * s1 download arr process
    
    * working process
    
    * updated readme inst

--- src/ceda_s1/find.py
+++ src/ceda_s1/find.py
@@ -8,25 +8,15 @@ import random
 import geopandas as gpd
 import numpy as np
 import rasterio as rio
-from rasterio.windows import from_bounds, shape
 from scipy.ndimage import label
-
-logging.basicConfig(
-    level=logging.INFO,
-    format="\n%(asctime)s.%(msecs)03d - %(message)s",
-    datefmt="%Y-%m-%d %H:%M:%S",
-    handlers=[
-                logging.FileHandler("s1_image_search.log", mode="w"),
-                logging.StreamHandler(),
-            ],
-)
+from shapely.geometry import box
+from tqdm import tqdm
 
 
 class FindS1:
     """
     Searches for Sentinel-1 data within a specified date range from CEDA archive
-    and filters results based on relative orbit numbers. Extracts links to tif files
-    from the available data folders.
+    and filters results based area of interest geometries.
     """
 
     def __init__(
@@ -34,8 +24,9 @@ class FindS1:
         start_date,
         end_date,
         aoi_filepath,
-        orbit_numbers=[30, 52, 103, 125, 132],
-        filter_orbits=False,
+        orbit_numbers=[52, 59, 74, 81, 103, 96, 125, 132, 161, 154, 1, 8, 30, 23],
+        date_images_list=None,
+        aoi_id="OBJECTID",
         max_no_data_patch=10,
     ):
         """
@@ -45,18 +36,28 @@ class FindS1:
             start_date (str): The start date for the search in the format 'YYYY-MM-DD'.
             end_date (str): The end date for the search in the format 'YYYY-MM-DD'.
             aoi_filepath (str): The filepath to shapefile, geojson or geopackage for area of interest.
+            aoi_id (str): The column name in the AOI file to use as the unique ID for search results.
             orbit_numbers (list): A list of relative orbit numbers to filter results by.
-            filter_orbits (bool): If True only one ascending and one descending orbit will be returned.
             max_no_data_patch (int): If a continuous region of no data pixels is larger than this value, the image is discarded.
         """
         self.start_date = start_date
         self.end_date = end_date
         self.orbit_numbers = orbit_numbers
-        self.filter_orbits = filter_orbits
+        self.date_images_list = date_images_list
         self.aoi_filepath = aoi_filepath
+        self.aoi_id = aoi_id
         self.max_no_data_patch = max_no_data_patch
         self.base_url = "https://data.ceda.ac.uk/neodc/sentinel_ard/data/sentinel_1"
-        self.aoi = self._read_aoi_file()
+
+        logging.basicConfig(
+            level=logging.INFO,
+            format="\n%(asctime)s.%(msecs)03d - %(message)s",
+            datefmt="%Y-%m-%d %H:%M:%S",
+            handlers=[
+                logging.FileHandler("s1_image_search.log", mode="w"),
+                logging.StreamHandler(),
+            ],
+        )
         self.__logger = logging.getLogger(__name__)
 
     def _read_aoi_file(self):
@@ -68,6 +69,7 @@ class FindS1:
         """
         try:
             aoi_data = gpd.read_file(self.aoi_filepath)
+            self.__logger.info(f"Read AOI file with {len(aoi_data)} features.")
             return aoi_data
         except Exception as e:
             self.__logger.error(f"Failed to read AOI file: {e}")
@@ -106,15 +108,15 @@ class FindS1:
         urls = []
         for current_date in date_range:
             check_url = self._create_date_url(current_date)
-            try:
-                response = requests.head(check_url, timeout=10)
-                if response.status_code == 200:
-                    urls.append(check_url)
-            except ReadTimeout:
-                self.__logger.error(
-                    f"Request timed out for {check_url}. \nSkipping this date..."
-                )
-        logging.info(f"Found {len(urls)} existing data folders in the date range.")
+            urls.append(check_url)
+            # try:
+            #     response = requests.head(check_url, timeout=10)
+            #     if response.status_code == 200:
+            #         urls.append(check_url)
+            # except ReadTimeout:
+            #     self.__logger.error(
+            #         f"Request timed out for {check_url}. \nSkipping this date..."
+            #     )
         return urls
 
     def _extract_links(self, url):
@@ -142,48 +144,11 @@ class FindS1:
                     if int(href.split("/")[-1].split("_")[2]) in self.orbit_numbers:
                         if "GB_OSGB" in href.split("/")[-1]:
                             img_links.append(href.replace("?download=1", ""))
-
         return set(img_links)
 
-    def _count_orbits(self, img_list):
+    def _aoi_check_images(self, img_list):
         """
-        Counts the number of images per orbit number for ascending and descending orbits.
-
-        Args:
-            img_list (list): A list of image links to count.
-
-        Returns:
-            tuple: Two lists of orbit numbers ordered by the most images for ascending and descending orbits.
-        """
-        asc_orbits = {}
-        desc_orbits = {}
-
-        for img_link in img_list:
-            parts = img_link.split("/")[-1].split("_")
-            orbit_number = int(parts[2])
-            orbit_type = parts[3]
-
-            if str.upper(orbit_type) == "ASC":
-                if orbit_number not in asc_orbits:
-                    asc_orbits[orbit_number] = 0
-                asc_orbits[orbit_number] += 1
-            elif str.upper(orbit_type) == "DESC":
-                if orbit_number not in desc_orbits:
-                    desc_orbits[orbit_number] = 0
-                desc_orbits[orbit_number] += 1
-
-        sorted_asc_orbits = dict(
-            sorted(asc_orbits.items(), key=lambda item: item[1], reverse=True)
-        )
-        sorted_desc_orbits = dict(
-            sorted(desc_orbits.items(), key=lambda item: item[1], reverse=True)
-        )
-
-        return sorted_asc_orbits, sorted_desc_orbits
-
-    def _spatial_check_images(self, img_list):
-        """
-        Checks if a window of the AOI geometry is entirely contained within the image.
+        Checks if a window of the AOI geometry is entirely within the image.
 
         Args:
             img_list (list): A list of image links to check.
@@ -191,21 +156,29 @@ class FindS1:
         Returns:
             str: The first image link that matches the criteria.
         """
-        retained_images = []
         aoi_gdf = self._read_aoi_file()
-        bounds = aoi_gdf.bounds.values[0]
-        for img_link in img_list:
+        retained_images = {int(uid): [] for uid in aoi_gdf[self.aoi_id].unique()}
+        orbits = []
+        for img_link in tqdm(img_list, desc="Checking aoi intersection of images"):
+            orbit = img_link.split("/")[-1].split("_")[2]
+            if orbit not in orbits:
+                orbits.append(orbit)
             try:
                 with rio.open(img_link) as src:
-                    window = from_bounds(*bounds, transform=src.transform)
-                    arr = src.read(window=window)
-                    if arr.shape[1] > 0 and arr.shape[2] > 0:
-                        retained_images.append(img_link)
-                        self.__logger.info(f"Image {img_link} retained.")
-                    else:
-                        self.__logger.info(f"Image {img_link} discarded.")
+                    img_geom = box(*src.bounds)
             except Exception as e:
                 self.__logger.error(f"Error reading image {img_link}: {e}")
+                continue
+            intersect_aoi = aoi_gdf[aoi_gdf.within(img_geom)]
+            if intersect_aoi.empty:
+                continue
+            ids = intersect_aoi[self.aoi_id].astype("uint16").to_list()
+            for id in ids:
+                current = retained_images[id]
+                current.append(img_link)
+                retained_images[id] = current
+
+        self.__logger.info(f"Found images for relative orbits {" ".join(orbits)}")
         return retained_images
 
     def _find_largest_region(self, binary_image):
@@ -229,7 +202,7 @@ class FindS1:
         # Find the largest connected component
         return int(component_sizes.max())
 
-    def _filter_nodata(self, img_list):
+    def _filter_nodata(self, arr, nodata_value):
         """
         Filters images based on the percentage of nodata pixels.
 
@@ -239,70 +212,12 @@ class FindS1:
         Returns:
             list: A list of image links that meet the specified criteria.
         """
-        retained_images = []
-        aoi_gdf = self._read_aoi_file()
-        bounds = aoi_gdf.bounds.values[0]
-        for img_link in img_list:
-            try:
-                with rio.open(img_link) as src:
-                    window = from_bounds(*bounds, transform=src.transform)
-                    arr = src.read(window=window)
-                    no_data_value = src.nodata
-                    binary_image = (arr[1] == no_data_value).astype(np.uint8)
-                    largest_region = self._find_largest_region(binary_image)
-                    if largest_region <= self.max_no_data_patch:
-                        retained_images.append(img_link)
-                        self.__logger.info(f"Image {img_link} retained.")
-                    else:
-                        self.__logger.info(f"Image {img_link} discarded.")
-            except Exception as e:
-                self.__logger.error(f"Error reading image {img_link}: {e}")
-        return retained_images
 
-    def _filter_image_extent(self, img_list):
-        """
-        Checks if the specified orbit numbers are present in the image list and if the image geometry
-        entirely contains the AOI geometry.
+        binary_image = (arr[0] == nodata_value).astype(np.uint8)
+        largest_region = self._find_largest_region(binary_image)
+        return largest_region <= self.max_no_data_patch
 
-        Args:
-            img_list (list): A list of image links to check.
-
-        Returns:
-            str: The first image link that matches the criteria.
-        """
-        self.__logger.info(f"Filtering {len(img_list)} images based on AOI window.")
-        img_list = self._spatial_check_images(img_list)
-        self.__logger.info(f"After AOI extent filter {len(img_list)} images.")
-
-        self.__logger.info(
-            f"Filtering {len(img_list)} images based on nodata pixel patches."
-        )
-        img_list = self._filter_nodata(img_list)
-        self.__logger.info(f"After no data filter {len(img_list)} images.")
-
-        sorted_asc_orbits, sorted_desc_orbits = self._count_orbits(img_list)
-
-        self.__logger.info(f"Order of asc orbits {sorted_asc_orbits}")
-        self.__logger.info(f"Order_of desc orbits {sorted_desc_orbits}")
-
-        if self.filter_orbits:
-
-            use_asc_orbit = list(sorted_asc_orbits.keys())[0]
-            use_desc_orbit = list(sorted_desc_orbits.keys())[0]
-            self.__logger.info(
-                f"Filtering to asc {use_asc_orbit} and desc {use_desc_orbit} orbits."
-            )
-
-            img_list = [
-                x
-                for x in img_list
-                if int(x.split("/")[-1].split("_")[2])
-                in [use_asc_orbit, use_desc_orbit]
-            ]
-
-        return img_list
-
-    def get_img_list(self):
+    def get_img_feature_dict(self):
         """
         Gathers all tif file links from URLs constructed based from base CEDA URL and a date range.
 
@@ -310,15 +225,24 @@ class FindS1:
         Filters links based on orbit numbers.
 
         Returns:
-            list: A list of all extracted tif image file links across the specified date range.
+            dict: A dictionary of feature id keys with lists of applicable images.
         """
+        self.__logger.info(
+            f"Searching for images between {self.start_date} and {self.end_date}"
+        )
         date_urls = self._get_existing_folders()
-        img_links = []
-        for url in date_urls:
-            img_links.extend(self._extract_links(url))
-            time.sleep(random.uniform(0.01, 0.1))
+        if not self.date_images_list:
+            img_links = []
+            for url in tqdm(date_urls, "Extracting image links from date folders"):
+                img_links.extend(self._extract_links(url))
+        else:
+            img_links = self.date_images_list
 
-        img_links = self._filter_image_extent(img_links)
-        self.__logger.info(f"Returning {len(img_links)} suitable image links.")
+        self.__logger.info(f"Checking {len(img_links)} image links.")
 
-        return img_links
+        aoi_img_dict = self._aoi_check_images(img_links)
+        self.__logger.info(
+            f"Returning image search results for {len(aoi_img_dict.keys())} features."
+        )
+
+        return aoi_img_dict, img_links
